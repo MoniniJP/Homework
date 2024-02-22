b359ffc7067aad8c56c20c304483d45b70e9d537
MoniniJP <mishaod1919@gmail.com>
2024-02-22 17:18:28 +0300

    Changes V1/0

--- src/Main.java
+++ src/Main.java
@@ -8,53 +8,53 @@ public class Main {
 
         double b;
         double y;
-        double x = 2;
+        x = 2;
         b = 4 * x * 3 - 12 * y;
         System.out.println("b= " + b);
 
         double c;
-        double x = 3;
-        double y = 1;
-        c = Math.sin(x * (-3 + 2y) + 1);
+        x = 3;
+        y = 1;
+        c = Math.sin(x * (-3 + 2*y) + 1);
         System.out.println("c=" + c);
 
         double d;
-        double x;
-        d = 122 Math.pow(x * -2);
+        x;
+        d = 122 * Math.pow(x * -2);
         System.out.println("d=" + d);
 
         double e;
-        double x = 2;
-        double y = 1;
+        x = 2;
+        y = 1;
 
         e = x * x * x * x * Math.abs(485 - 179 * y);
 
         System.out.println("e=" + e);
 
         double f;
-        double x = 2;
+        x = 2;
         f = 3 + 9 * Math.pow(x) - 5 / x - 1;
         System.out.println("f=" + f);
 
 
         double g;
-        double x = 2;
+        x = 2;
 
         g = (2.5 * (4 - 6 * x)) - 5;
         System.out.println("g=" + g);
 
 
         double h;
-        double x;
+        x;
         h = Math.cos(x / 2) + Math.sin(x / 4) + 3 * x / 6;
 
         System.out.println("h=" + h);
 
 
-        double y;
-        double x;
+        y;
+        x;
 
-        y = Math.abs(5 * x - 3 / Math.pow(x * x) + 6 + 7Math.pow(x * x * x) - 12);
+        y = Math.abs(5 * x - 3 / Math.pow(x * x) + 6 + 7*Math.pow* (x * x * x) - 12);
 
         System.out.println("y=" + y);
 
