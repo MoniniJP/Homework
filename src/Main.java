2015a9b689f84d9560e202f3bfbbdec80db3151d
MoniniJP <mishaod1919@gmail.com>
2024-02-25 12:31:13 +0200

    Changes V1/0

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 
         double b;
-        double y;
+        double y = 5;
         x = 2;
         b = 4 * x * 3 - 12 * y;
         System.out.println("b= " + b);
@@ -15,49 +15,41 @@ public class Main {
         double c;
         x = 3;
         y = 1;
-        c = Math.sin(x * (-3 + 2*y) + 1);
+        c = Math.sin(x * (-3 + 2 * y) + 1);
         System.out.println("c=" + c);
 
         double d;
-        x;
-        d = 122 * Math.pow(x * -2);
+        d = 122 * Math.pow(x, -2);
         System.out.println("d=" + d);
 
         double e;
         x = 2;
-        y = 1;
 
         e = x * x * x * x * Math.abs(485 - 179 * y);
 
         System.out.println("e=" + e);
 
         double f;
-        x = 2;
-        f = 3 + 9 * Math.pow(x) - 5 / x - 1;
+        f = 3 + Math.pow(9, x) - 5 / x - 1;
         System.out.println("f=" + f);
 
 
         double g;
-        x = 2;
-
         g = (2.5 * (4 - 6 * x)) - 5;
         System.out.println("g=" + g);
 
 
         double h;
-        x;
         h = Math.cos(x / 2) + Math.sin(x / 4) + 3 * x / 6;
 
         System.out.println("h=" + h);
 
 
-        y;
-        x;
-
-        y = Math.abs(5 * x - 3 / Math.pow(x * x) + 6 + 7*Math.pow* (x * x * x) - 12);
+        x = 1;
+        y = Math.abs(5 * x - 3 / Math.pow(x, 2) + 6 + 7 * Math.pow(x, 3) - 12);
 
         System.out.println("y=" + y);
 
     }
 }
-    }
+
